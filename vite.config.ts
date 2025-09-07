362b67e1ed2350c5991c8a1459459157c36553a4
Mastore5555 <xmarcelasilva2@outlook.com>
2025-09-07 12:11:12 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/o-resgate/", // 👈 caminho do repositório
+  base: "resgate-da-presenca", // 👈 caminho do repositório
 });
